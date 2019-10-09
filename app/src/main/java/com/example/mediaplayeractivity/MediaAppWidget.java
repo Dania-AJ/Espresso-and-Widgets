e7f335a0a1a84d002fe630a0bd3c733d39701269
uconnect-android <dania.abedaljawad@udacity.com>
2019-10-09 14:38:39 +0300

    solution

---
+++ app/src/main/java/com/example/mediaplayeractivity/MediaAppWidget.java
@@ -0,0 +1,49 @@
+package com.example.mediaplayeractivity;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class MediaAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        //TODO 1: Create an Intent to launch MainActivity then add it to a PendingIntent
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        //TODO 2 : Get the layout of the App Widget and inflate it into a RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.media_app_widget);
+
+        //TODO 3 : Call setOnClickPendingIntent on the RemoteViews object and provide the view that will be clicked and the pending intent to be launched
+        views.setOnClickPendingIntent(R.id.media_image, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
